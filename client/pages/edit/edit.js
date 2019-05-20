0e2abd2890997d4a3d866d50c89795d0624715eb
Bojan <qc_60c537f462dc46f3bacab79e3535a173@git.code.tencent.com>
2019-05-20 22:52:20 +0800

    自定义源页面数据处理

--- client/pages/edit/edit.js
+++ client/pages/edit/edit.js
@@ -1,10 +1,19 @@
 Page({
   data: {
-    value: ''
+    editSource: [], 
   },
-
-  onChange(event) {
-    // event.detail 为当前输入的值
-    console.log(event.detail);
+  addTo:function(e) {
+    var that = this;
+    var editSource = [];
+    editSource[0] = e.detail.value.address;
+    editSource[1] = e.detail.value.name;
+    editSource[2] = e.detail.value.decribe;
+    editSource[3] = e.detail.value.logo;
+    editSource[4] = e.detail.value.source;
+  },
+  addCancel:function(e) {
+    wx.redirectTo({
+      url: '/pages/article/article'
+    })
   }
 });
