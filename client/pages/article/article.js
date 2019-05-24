c6136afd2ca00ad4525c64f88d0cbf8b2cdfb0f0
Bojan <qc_60c537f462dc46f3bacab79e3535a173@git.code.tencent.com>
2019-05-25 00:38:08 +0800

    文章详情页面完成

--- client/pages/article/article.js
+++ client/pages/article/article.js
@@ -11,47 +11,88 @@ const article = '';
 
 Page({
   data: {
+    index: '',
     title: '',          // 文章标题
     author: '',         // 文章作者或源名称
     favicon: '',        // 源logo
     link: '',           // 原文链接
-    pubDate: '',        // 更新日期
+    rssUrl: '',         // rss源
+    description: '',    // 原文内容
+    published: '',        // 更新日期
     nodes: article,     // 富文本转化变量
     showPopup: false,   // 链接提示弹框，由于个人开发者不具备webview的权限，如果读者想看更多内容，这里提供链接，需到浏览器访问
     linkSrc: '',        // 链接提示弹框的链接数据
   },
   onLoad: function (options) {
     // 加载页面后，用跨页参数在缓存中查询出具体文字内容
-    this.showDetail(options.id, options.favicon);
+    this.data.index = options.index;
+    this.data.favicon = options.favicon;
+    this.data.rssUrl = options.rssUrl;
+    this.showDetail();
   },
 
   // 展示文章详情
-  showDetail: function (id, favicon) {
-    const rssData = wx.getStorageSync('rssData') || {};
+  showDetail: function () {
+    // const rssData = wx.getStorageSync('rssData') || {};
     // const author = rssData.title || '';
-
+    let that = this;
+    wx.request({
+      url:    'http://nkurss.potatobrother.cn:8080/rssread/onePost',
+      method: 'GET',
+      data: {
+        rssUrl: 'https://zhihu.com/rss',
+        index: 'http://www.zhihu.com/question/284593685/answer/693527322'
+      },
+      header: {
+        "content-type": "application/x-www-form-urlencoded"
+      },
+      success: function (res) {
+        console.log(res)
+        var tempOnePost = res.data.onePost
+        console.log(tempOnePost)
+        if (!tempOnePost) {
+          console.log("no_matched_post")
+        }
+        else if(tempOnePost=="error_request"){
+          console.log("error_request")
+        }else{
+          that.setData({
+            title: tempOnePost.title,
+            author: tempOnePost.author,
+            link: tempOnePost.link,
+            description: tempOnePost.summary_detail.value,
+            published: tempOnePost.published ? util.formatDate("yyyy-MM-dd HH:mm:ss", tempOnePost.published) : '',
+          })
+          console.log("success");
+          // 调用富文本转化方法 html
+          WxParse.wxParse('article', 'html', that.data.description, that, 5);
+        }
+      },
+      fail: function (res) {
+        console.log('fail');
+      },
+      complete: function (res) {
+        console.log('complete');
+      }
+    })
     // const favicon = `${rssData.link}/favicon.ico`;
-    const rssDataItem = rssData.item[id];
-    const { title, link, pubDate } = rssDataItem;
+    // const rssDataItem = rssData.item[id];
+    // const { title, link, pubDate } = rssDataItem;
     // console.log(rssDataItem);
-    const that = this;
-    this.setData({
-      title:rssDataItem.title,
-      author:rssDataItem.author,
-      favicon:rssDataItem.favicon,
-      link:rssDataItem.link,
-      pubDate: pubDate ? util.formatDate("yyyy-MM-dd HH:mm:ss", pubDate) : '', // 日期时间需格式化
-    });
+    // this.setData({
+    //   title,
+    //   author,
+    //   link,
+    //   description,
+    //   pubDate: pubDate ? util.formatDate("yyyy-MM-dd HH:mm:ss", pubDate) : '', // 日期时间需格式化
+    // });
 
     // 多种rss返回值数据的简单兼容处理，待优化（TODO）
-    if (rssDataItem['content:encoded']) {
-      // 调用富文本转化方法
-      WxParse.wxParse('article', 'html', rssDataItem['content:encoded'], that, 5);
-      return;
-    }
-
-    // 调用富文本转化方法 html
-    WxParse.wxParse('article', 'html', rssDataItem.description, that, 5);
+    // if (tempOnePost['content:encoded']) {
+    //   // 调用富文本转化方法
+    //   WxParse.wxParse('article', 'html', tempOnePost['content:encoded'], that, 5);
+    //   return;
+    // }
   },
 
   // 链接弹框显隐控制
