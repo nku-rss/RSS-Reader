b8658ab2fa6449971203500366487acc4ab61b81
Bojan <qc_60c537f462dc46f3bacab79e3535a173@git.code.tencent.com>
2019-05-30 21:36:55 +0800

    article页面前端更新

--- client/pages/article/article.js
+++ client/pages/article/article.js
@@ -22,11 +22,16 @@ Page({
     postId: '',
     title: '',          // 文章标题
     author: '',         // 文章作者或源名称
-    favicon: '',        // 源logo
-    link: '',           // 原文链接
+    // favicon: '',        // 源logo
+    // link: '',           // 原文链接
     rssUrl: '',         // rss源
     description: '',    // 原文内容
-    published: '',        // 更新日期
+    // published: '',        // 更新日期
+
+    // ----change published to time----
+    time:'',
+    // ----change close----
+
     nodes: article,     // 富文本转化变量
     showPopup: false,   // 链接提示弹框，由于个人开发者不具备webview的权限，如果读者想看更多内容，这里提供链接，需到浏览器访问
     linkSrc: '',        // 链接提示弹框的链接数据
@@ -102,8 +107,11 @@ Page({
       success: function (res) {
         //console.log(res)
         var tempOnePost = res.data.onePost
-        var description_value
-        var index = 0
+
+        // ----remove useless value
+        // var description_value
+        // var index = 0
+
         console.log(tempOnePost)
         if (!tempOnePost) {
           console.log("no_matched_post")
@@ -111,25 +119,33 @@ Page({
         else if(tempOnePost=="error_request"){
           console.log("error_request")
         }else{
-          if (tempOnePost.summary_detail != undefined){
-            description_value = tempOnePost.summary_detail.value
-          } else if (tempOnePost.content != undefined){
-            // description_value = tempOnePost.content[0].value
-            while (tempOnePost.content[index] != undefined) {
-              description_value += tempOnePost.content[index].value
-              index ++
-            }
-          } else if (tempOnePost.description != undefined){
-            description_value = tempOnePost.description
-          } else {
-            description_value = tempOnePost.summary
-          }
+          //----remove methods for handle the content----
+          // if (tempOnePost.summary_detail != undefined){
+          //   description_value = tempOnePost.summary_detail.value
+          //   description_value.replace(/SRC/g,"src");
+          // } else if (tempOnePost.content != undefined){
+          //   // description_value = tempOnePost.content[0].value
+          //   while (tempOnePost.content[index] != undefined) {
+          //     description_value += tempOnePost.content[index].value
+          //     index ++
+          //   }
+          // } else if (tempOnePost.description != undefined){
+          //   description_value = tempOnePost.description
+          // } else {
+          //   description_value = tempOnePost.summary
+          // }
+
           that.setData({
             title: tempOnePost.title,
             author: tempOnePost.author,
-            link: tempOnePost.link,
-            description: description_value,
-            published: tempOnePost.published ? util.formatDate("yyyy-MM-dd HH:mm:ss", tempOnePost.published) : '',
+
+            //----change description and time value----
+            
+            description: tempOnePost.content,
+            time: tempOnePost.time ? util.formatDate("yyyy-MM-dd HH:mm:ss", tempOnePost.time) : '',
+            // description: description_value,
+            // published: tempOnePost.published ? util.formatDate("yyyy-MM-dd HH:mm:ss", tempOnePost.published) : '',
+            //----change close----
           })
           console.log("success");
           // 调用富文本转化方法 html
